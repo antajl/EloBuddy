6d6c11936340d116029ea7242117a35b35a328f1
Bloodimir <fundyourself@hotmail.com>
2015-10-04 05:29:05 +0300

    Bloodimir Tryndamere 1.0.0.0 Release

---
+++ Bloodimir Tryndamere/Combo.cs
@@ -0,0 +1,98 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu.Values;
+
+namespace Bloodimir_Tryndamere
+{
+    internal class Combo
+    {
+        public enum AttackSpell
+        {
+            E,
+            W
+        };
+
+        public static AIHeroClient Tryndamere
+        {
+            get { return ObjectManager.Player; }
+        }
+
+        public static void TrynCombo()
+        {
+            var WCHECK = Program.ComboMenu["usecombow"].Cast<CheckBox>().CurrentValue;
+            var ECHECK = Program.ComboMenu["usecomboe"].Cast<CheckBox>().CurrentValue;
+            var WREADY = Program.W.IsReady();
+            var EREADY = Program.E.IsReady();
+
+            if (ECHECK && EREADY)
+            {
+                var enemy = TargetSelector.GetTarget(Program.E.Range, DamageType.Physical);
+                if (enemy != null)
+                    if (Program.E.IsReady() &&
+                        Tryndamere.Distance(enemy) <= Program.E.Range - Player.Instance.GetAutoAttackRange())
+                    {
+                        Program.E.Cast(enemy.ServerPosition);
+                    }
+            }
+
+            if (WCHECK && WREADY)
+            {
+                var wenemy = TargetSelector.GetTarget(Program.W.Range, DamageType.Magical);
+                {
+                    if (!wenemy.IsFacing(Program.Tryndamere))
+                    {
+                        Program.W.Cast();
+                    }
+                    if (Orbwalker.CanAutoAttack)
+                    {
+                        var enemy = TargetSelector.GetTarget(Player.Instance.GetAutoAttackRange(), DamageType.Physical);
+
+                        if (enemy != null)
+                            Orbwalker.ForcedTarget = enemy;
+                    }
+                }
+            }
+        }
+
+        public static
+            void Items()
+        {
+            var ienemy = TargetSelector.GetTarget(Player.Instance.GetAutoAttackRange() + 425,
+                DamageType.Physical);
+            if (ienemy != null)
+            {
+                if (ienemy.IsValid && !ienemy.IsZombie)
+                {
+                    if (Program.MiscMenu["usebotrk"].Cast<CheckBox>().CurrentValue)
+                    {
+                        if (Program.botrk.IsOwned() && Program.botrk.IsReady() &&
+                            Program.botrk.IsInRange(ienemy))
+                            Program.botrk.Cast(ienemy);
+                    }
+                    if (Program.MiscMenu["usebilge"].Cast<CheckBox>().CurrentValue)
+                    {
+                        if (Program.bilgewater.IsOwned() && Program.bilgewater.IsReady())
+                            Program.bilgewater.Cast(ienemy);
+                    }
+                    if (Program.MiscMenu["usehydra"].Cast<CheckBox>().CurrentValue)
+                    {
+                        if (Program.hydra.IsOwned() && Program.hydra.IsReady() &&
+                            Program.hydra.IsInRange(ienemy))
+                            Program.hydra.Cast();
+                    }
+                    if (Program.MiscMenu["useTiamat"].Cast<CheckBox>().CurrentValue)
+                    {
+                        if (Program.tiamat.IsOwned() && Program.tiamat.IsReady() &&
+                            Program.tiamat.IsInRange(ienemy))
+                            Program.tiamat.Cast();
+                    }
+                    if (Program.MiscMenu["useyoumuu"].Cast<CheckBox>().CurrentValue)
+                    {
+                        if (Program.youmuu.IsOwned() && Program.youmuu.IsReady())
+                            Program.youmuu.Cast();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Bloodimir Tryndamere/Program.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace Bloodimir_Tryndamere
+{
+    internal class Program
+    {
+        public static Spell.Active Q;
+        public static Spell.Active W;
+        public static Spell.Skillshot E;
+        public static Spell.Active R;
+        public static Menu TrynMenu, ComboMenu, DrawMenu, SkinMenu, MiscMenu, LaneJungleClear;
+        public static Item tiamat, hydra, bilgewater, youmuu, botrk;
+        public static AIHeroClient Tryndamere = ObjectManager.Player;
+
+        public static AIHeroClient _Player
+        {
+            get { return ObjectManager.Player; }
+        }
+
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += OnLoaded;
+        }
+
+        private static void OnLoaded(EventArgs args)
+        {
+            if (Player.Instance.ChampionName != "Tryndamere")
+                return;
+            Bootstrap.Init(null);
+            Q = new Spell.Active(SpellSlot.Q);
+            W = new Spell.Active(SpellSlot.W, 400);
+            E = new Spell.Skillshot(SpellSlot.E, 659, SkillShotType.Linear, (int) 250f, (int) 700f, (int) 92.5f);
+            R = new Spell.Active(SpellSlot.R);
+
+            botrk = new Item(3153, 550f);
+            bilgewater = new Item(3144, 475f);
+            hydra = new Item(3074, 250f);
+            tiamat = new Item(3077, 250f);
+            youmuu = new Item(3142, 10);
+
+            TrynMenu = MainMenu.AddMenu("BloodimirTryndamere", "bloodimirtryndamere");
+            TrynMenu.AddGroupLabel("Bloodimir.Tryndamere");
+            TrynMenu.AddSeparator();
+            TrynMenu.AddLabel("Bloodimir Tryndamere V1.0.0.0");
+
+            ComboMenu = TrynMenu.AddSubMenu("Combo", "sbtw");
+            ComboMenu.AddGroupLabel("Combo Settings");
+            ComboMenu.AddSeparator();
+            ComboMenu.Add("usecomboq", new CheckBox("Auto Q"));
+            ComboMenu.Add("usecombow", new CheckBox("Use W"));
+            ComboMenu.Add("usecomboe", new CheckBox("Use E "));
+            ComboMenu.Add("usecombor", new CheckBox("Auto R"));
+            ComboMenu.AddSeparator();
+            ComboMenu.Add("rslider", new Slider("Minimum health for R", 15, 0, 95));
+            ComboMenu.AddSeparator();
+            ComboMenu.Add("qhp", new Slider("Q % HP", 25, 0, 95));
+
+
+            DrawMenu = TrynMenu.AddSubMenu("Drawings", "drawings");
+            DrawMenu.AddGroupLabel("Drawings");
+            DrawMenu.AddSeparator();
+            DrawMenu.Add("drawe", new CheckBox("Draw E"));
+
+            LaneJungleClear = TrynMenu.AddSubMenu("Lane Jungle Clear", "lanejungleclear");
+            LaneJungleClear.AddGroupLabel("Lane Jungle Clear Settings");
+            LaneJungleClear.Add("LCE", new CheckBox("Use E"));
+
+            MiscMenu = TrynMenu.AddSubMenu("Misc Menu", "miscmenu");
+            MiscMenu.AddGroupLabel("Misc");
+            MiscMenu.AddSeparator();
+            MiscMenu.Add("kse", new CheckBox("KS using E"));
+            MiscMenu.Add("ksbotrk", new CheckBox("KS using Botrk"));
+            MiscMenu.Add("kshydra", new CheckBox("KS using Hydra"));
+            MiscMenu.Add("usehydra", new CheckBox("Use Hydra"));
+            MiscMenu.Add("usetiamat", new CheckBox("Use Tiamat"));
+            MiscMenu.Add("usebotrk", new CheckBox("Use Botrk"));
+            MiscMenu.Add("usebilge", new CheckBox("Use Bilgewater"));
+            MiscMenu.Add("useyoumuu", new CheckBox("Use Youmuu"));
+
+
+            SkinMenu = TrynMenu.AddSubMenu("Skin Changer", "skin");
+            SkinMenu.AddGroupLabel("Choose the desired skin");
+
+            var skinchange = SkinMenu.Add("skinid", new Slider("Skin", 4, 0, 7));
+            var skinid = new[]
+            {"Default", "Highland", "King", "Viking", "Demon Blade", "Sultan", "Warring Kingdoms", "Nightmare"};
+            skinchange.DisplayName = skinid[skinchange.CurrentValue];
+            skinchange.OnValueChange += delegate(ValueBase<int> sender, ValueBase<int>.ValueChangeArgs changeArgs)
+            {
+                sender.DisplayName = skinid[changeArgs.NewValue];
+                if (MiscMenu["debug"].Cast<CheckBox>().CurrentValue)
+                {
+                    Chat.Print("skin-changed");
+                }
+            };
+            Game.OnTick += Tick;
+            Drawing.OnDraw += OnDraw;
+        }
+
+        public static void AutoQ(bool useR)
+        {
+            var autoQ = ComboMenu["usecomboq"].Cast<CheckBox>().CurrentValue;
+            var healthAutoR = ComboMenu["qhp"].Cast<Slider>().CurrentValue;
+            if (autoQ && _Player.HealthPercent < healthAutoR)
+            {
+                Q.Cast();
+            }
+        }
+
+        public static void AutoUlt(bool useR)
+        {
+            var autoR = ComboMenu["usecombor"].Cast<CheckBox>().CurrentValue;
+            var healthAutoR = ComboMenu["rslider"].Cast<Slider>().CurrentValue;
+            if (autoR && _Player.HealthPercent < healthAutoR)
+                if (
+                    ObjectManager.Get<AIHeroClient>()
+                        .Where(x => x.IsEnemy && x.Distance(Tryndamere.Position) <= 1100)
+                        .Count() >= 1)
+                {
+                    R.Cast();
+                }
+        }
+
+        private static void OnDraw(EventArgs args)
+        {
+            if (!Tryndamere.IsDead)
+            {
+                if (DrawMenu["drawe"].Cast<CheckBox>().CurrentValue && E.IsLearned)
+                {
+                    Drawing.DrawCircle(Tryndamere.Position, E.Range, Color.DarkBlue);
+                }
+            }
+        }
+
+        public static void Flee()
+        {
+            Orbwalker.MoveTo(Game.CursorPos);
+            E.Cast(Game.CursorPos);
+        }
+
+        private static void Tick(EventArgs args)
+        {
+            Killsteal();
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Flee))
+            {
+                Flee();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            {
+                Combo.TrynCombo();
+                Combo.Items();
+            }
+            {
+                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear) ||
+                    Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear))
+                {
+                    LaneJungleClearA.LaneClear();
+                }
+                SkinChange();
+                AutoUlt(ComboMenu["usecombor"].Cast<CheckBox>().CurrentValue);
+                AutoQ(ComboMenu["usecomboq"].Cast<CheckBox>().CurrentValue);
+            }
+        }
+
+        private static void Killsteal()
+        {
+            if (MiscMenu["kse"].Cast<CheckBox>().CurrentValue && E.IsReady())
+            {
+                try
+                {
+                    foreach (
+                        var etarget in
+                            HeroManager.Enemies.Where(
+                                hero => hero.IsValidTarget(E.Range) && !hero.IsDead && !hero.IsZombie))
+                    {
+                        if (Tryndamere.GetSpellDamage(etarget, SpellSlot.E) >= etarget.Health)
+                        {
+                            {
+                                E.Cast(etarget.ServerPosition);
+                            }
+                            if (MiscMenu["ksbotrk"].Cast<CheckBox>().CurrentValue && botrk.IsReady() ||
+                                bilgewater.IsReady() || tiamat.IsReady())
+                            {
+                                {
+                                    try
+                                    {
+                                        foreach (
+                                            var itarget in
+                                                HeroManager.Enemies.Where(
+                                                    hero =>
+                                                        hero.IsValidTarget(botrk.Range) && !hero.IsDead &&
+                                                        !hero.IsZombie))
+                                        {
+                                            if (Tryndamere.GetItemDamage(itarget, ItemId.Blade_of_the_Ruined_King) >=
+                                                itarget.Health)
+                                            {
+                                                {
+                                                    botrk.Cast(itarget);
+                                                }
+                                                if (MiscMenu["kshydra"].Cast<CheckBox>().CurrentValue && botrk.IsReady() ||
+                                                    bilgewater.IsReady() || tiamat.IsReady())
+                                                {
+                                                    {
+                                                        try
+                                                        {
+                                                            foreach (
+                                                                var htarget in
+                                                                    HeroManager.Enemies.Where(
+                                                                        hero =>
+                                                                            hero.IsValidTarget(hydra.Range) &&
+                                                                            !hero.IsDead && !hero.IsZombie))
+                                                            {
+                                                                if (
+                                                                    Tryndamere.GetItemDamage(itarget,
+                                                                        ItemId.Ravenous_Hydra_Melee_Only) >=
+                                                                    itarget.Health)
+                                                                {
+                                                                    {
+                                                                        hydra.Cast();
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                        catch
+                                                        {
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                    catch
+                                    {
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                        catch
+                        {
+                }
+            }
+        }
+
+        private static
+            void SkinChange
+            ()
+        {
+            var style = SkinMenu["skinid"].DisplayName;
+            switch (style)
+            {
+                case "Default":
+                Player.SetSkinId(0);
+                break;
+                case "Highland":
+                Player.SetSkinId(1);
+                break;
+                case "King":
+                Player.SetSkinId(2);
+                break;
+                case "Viking":
+                Player.SetSkinId(3);
+                break;
+                case "Demon Blade":
+                Player.SetSkinId(4);
+                break;
+                case "Sultan":
+                Player.SetSkinId(5);
+                break;
+                case "Warring Kingdoms":
+                Player.SetSkinId(5);
+                break;
+                case "Nightmare":
+                Player.SetSkinId(5);
+                break;
+            }
+                            }
+    }
+}

941840e8fa7002d6b6abd36b4300a814420e484a
Bloodimir <fundyourself@hotmail.com>
2015-10-11 19:43:18 +0300

    New EB Core Update Fix

--- Evelynn/LastHitA.cs
+++ Evelynn/LastHitA.cs
@@ -29,7 +29,7 @@ namespace Evelynn
         {
             switch (t)
             {
-                case GameObjectType.AIHeroClient:
+                case GameObjectType.obj_AI_Hero:
                     return EntityManager.Heroes.Enemies.OrderBy(a => a.Health).FirstOrDefault(
                         a => a.Distance(Player.Instance) < range && !a.IsDead && !a.IsInvulnerable);
                 default:

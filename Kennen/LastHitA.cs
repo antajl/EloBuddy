6a9140641baddf7110502dc84b021d8c172f8a4d
Bloodimir <fundyourself@hotmail.com>
2015-10-11 21:14:41 +0300

    New EB Core Update Fix

--- Kennen/LastHitA.cs
+++ Kennen/LastHitA.cs
@@ -23,7 +23,7 @@ namespace Kennen
         {
             switch (t)
             {
-                case GameObjectType.obj_AI_Hero:
+                case GameObjectType.AIHeroClient:
                     return EntityManager.Heroes.Enemies.OrderBy(a => a.Health).FirstOrDefault(
                         a => a.Distance(Player.Instance) < range && !a.IsDead && !a.IsInvulnerable);
                 default:

060cb5849f3bceef73c48e9ebc9daa842e88f544
Bloodimir <fundyourself@hotmail.com>
2015-10-13 21:27:53 +0300

    Bloodimir Sona Release

---
+++ Bloodimir Sona/LaneJungleClearA.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu.Values;
+
+namespace Bloodimir_Sona
+{
+    internal class LaneJungleClearA
+    {
+        public enum AttackSpell
+        {
+            Q
+        };
+
+        public static AIHeroClient Sona
+        {
+            get { return ObjectManager.Player; }
+        }
+
+        public static Obj_AI_Base GetEnemy(float range, GameObjectType t)
+        {
+            switch (t)
+            {
+                case GameObjectType.AIHeroClient:
+                    return EntityManager.Heroes.Enemies.OrderBy(a => a.Health).FirstOrDefault(
+                        a => a.Distance(Player.Instance) < range && !a.IsDead && !a.IsInvulnerable);
+                default:
+                    return EntityManager.MinionsAndMonsters.EnemyMinions.OrderBy(a => a.Health).FirstOrDefault(
+                        a => a.Distance(Player.Instance) < range && !a.IsDead && !a.IsInvulnerable);
+            }
+        }
+        public static void LaneClear()
+        {
+            var QCHECK = Program.LaneJungleClear["LCQ"].Cast<CheckBox>().CurrentValue;
+            var QREADY = Program.Q.IsReady();
+
+            if (QCHECK && QREADY)
+            {
+                var qenemy = (Obj_AI_Minion)GetEnemy(Program.Q.Range, GameObjectType.obj_AI_Minion);
+
+                if (qenemy != null)
+                    {
+                        Program.Q.Cast();
+                    }
+                if (Orbwalker.CanAutoAttack)
+                {
+                    var enemy = (Obj_AI_Minion)GetEnemy(Sona.GetAutoAttackRange(), GameObjectType.obj_AI_Minion);
+
+                    if (enemy != null)
+                        Orbwalker.ForcedTarget = enemy;
+                }
+            }
+        }
+    }
+}

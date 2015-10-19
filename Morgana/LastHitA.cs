df75805be27b4892b3aaee5351477351c6fdef8e
Bloodimir <fundyourself@hotmail.com>
2015-10-19 21:27:00 +0300

    Last Hit Fix

--- Morgana/LastHitA.cs
+++ Morgana/LastHitA.cs
@@ -17,35 +17,37 @@ namespace Morgana
         {
             get { return ObjectManager.Player; }
         }
-        public static Obj_AI_Base GetEnemy(float range, GameObjectType t)
+
+        public static float Qcalc(Obj_AI_Base target)
         {
-            switch (t)
-            {
-                case GameObjectType.AIHeroClient:
-                    return EntityManager.Heroes.Enemies.OrderBy(a => a.Health).FirstOrDefault(
-                        a => a.Distance(Player.Instance) < range && !a.IsDead && !a.IsInvulnerable);
-                default:
-                    return EntityManager.MinionsAndMonsters.EnemyMinions.OrderBy(a => a.Health).FirstOrDefault(
-                        a => a.Distance(Player.Instance) < range && !a.IsDead && !a.IsInvulnerable);
-            }
+            return Morgana.CalculateDamageOnUnit(target, DamageType.Magical,
+                (new float[] {0, 80, 135, 190, 245, 300}[Program.Q.Level] + (0.90f*Morgana.FlatMagicDamageMod)));
         }
+
+        public static Obj_AI_Base MinionLh(GameObjectType type, AttackSpell spell)
+        {
+            return
+                ObjectManager.Get<Obj_AI_Base>()
+                    .OrderBy(a => a.Health)
+                    .FirstOrDefault(
+                        a =>
+                            a.IsEnemy && a.Type == type && a.Distance(Morgana) <= Program.Q.Range && !a.IsDead &&
+                            !a.IsInvulnerable && a.IsValidTarget(Program.Q.Range) && a.Health <= Qcalc(a));
+        }
+
         public static void LastHitB()
         {
             var qcheck = Program.LastHit["LHQ"].Cast<CheckBox>().CurrentValue;
             var qready = Program.Q.IsReady();
-
-           if (qcheck || qready)
+            if (qcheck || qready)
             {
-                var minion = (Obj_AI_Minion) GetEnemy(Program.Q.Range, GameObjectType.obj_AI_Minion);
+                var minion = (Obj_AI_Minion) MinionLh(GameObjectType.obj_AI_Minion, AttackSpell.Q);
                 if (minion != null)
                     if (Program.Q.MinimumHitChance >= HitChance.Low)
-                {
-                    Program.Q.Cast(minion.ServerPosition);
-                }
-            if (minion != null)
-            {
+                    {
+                        Program.Q.Cast(minion.ServerPosition);
+                    }
             }
         }
-    }
     }
 }

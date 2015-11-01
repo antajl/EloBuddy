24318b5bae3f722f31191cb23d9b6925b57f5e5a
Bloodimir <fundyourself@hotmail.com>
2015-11-01 13:34:29 +0200

    Q damage fix

--- Evelynn/LastHitA.cs
+++ Evelynn/LastHitA.cs
@@ -18,12 +18,14 @@ namespace Evelynn
             get { return ObjectManager.Player; }
         }
 
-
         public static float Qcalc(Obj_AI_Base target)
         {
-            return Evelynn.CalculateDamageOnUnit(target, DamageType.Magical,
-                (new float[] { 0, 80, 115, 150, 185, 220 }[Program.Q.Level] +
-                 (0.80f * Evelynn.FlatMagicDamageMod)));
+            {
+                return Evelynn.CalculateDamageOnUnit(target, DamageType.Magical,
+                    new float[] {0, 40, 50, 60, 70, 80}[Program.Q.Level] +
+                    new float[] {0, 35, 40, 45, 50, 55}[Program.Q.Level]/100*Player.Instance.FlatMagicDamageMod +
+                    new float[] {0, 50, 55, 60, 65, 70}[Program.Q.Level]/100*Player.Instance.FlatPhysicalDamageMod);
+            }
         }
 
         public static Obj_AI_Base GetEnemy(float range, GameObjectType t)
